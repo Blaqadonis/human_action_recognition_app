94e5912ddf5b95b6697c95f1c6a2b870ac82efac
Blaq <chinonsoodiaka@gmail.com>
2023-07-27 22:39:35 +0100

    edits

--- webservice/server/register_model.py
+++ webservice/server/register_model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import mlflow
 from mlflow.entities import ViewType
 from mlflow.tracking import MlflowClient
@@ -17,3 +18,24 @@ run_id = best_run.info.run_id
 model_uri = f"runs:/{run_id}/model"
 MODEL_NAME = "best_tensorflow_pretrained_model"
 mlflow.register_model(model_uri, MODEL_NAME)
+
+
+VERSION = client.get_latest_versions(name=MODEL_NAME)[0].version
+NEW_STAGE = 'Production'
+
+def transition_model_to_staging(name: str, version: int, stage: str):
+    '''Transition model to production stage'''
+    client.transition_model_version_stage(name=name, version=version,
+        stage=stage, archive_existing_versions=False )
+    return print("Model transitioned to production stage")
+
+
+
+
+
+if __name__ == "__main__":
+    transition_model_to_staging(MODEL_NAME, VERSION, NEW_STAGE)
+    date = datetime.today().date()
+    client.update_model_version(name=MODEL_NAME, version = VERSION,
+    description=f"The model version {VERSION} was transitioned to {NEW_STAGE} on {date}")
+    
